1cbb2bbcaab6ec4ebdca207b90218d1a52e884e2
Pravin Sharma <Pravin.Sharma@lntinfotech.com>
2021-03-03 19:17:13 +0530

    Converted Navbar and UserItem to Functional component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
-    render() {
-        const { login, avatar_url, html_url } = this.props.user;
+const UserItem = (props) =>  {
+        const { login, avatar_url, html_url } = props.user;
         return (
             <div className='card text-center'>
                 <img src={avatar_url} alt={login} className='round-img ' style={{ width: '60px' }} />
@@ -10,7 +10,11 @@ class UserItem extends Component {
                 <a href={html_url} className='btn btn-dark btn-sm'>More</a>
             </div>
         )
-    }
 }
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
+}
+
 
 export default UserItem
