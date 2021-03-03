0818dbef21fbb86a73d83d288861f5e43e5e2e9a
Pravin Sharma <Pravin.Sharma@lntinfotech.com>
2021-03-03 20:06:15 +0530

    added api to get users

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React, { Component } from 'react'
 import './App.css';
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 
 class App extends Component {
+
+  state ={
+    users: [],
+    isLoading: false 
+  }
+
+  async componentDidMount(){
+    this.setState({isLoading:true});
+
+    const result = await axios.get('https://api.github.com/users')
+
+    this.setState({users:result.data, isLoading:false});
+
+  }
+
   render() {
     return (
       <div className="App" >
         < Navbar />
         <div className='container'>
-          <Users />
+          <Users users={this.state.users} isLoading={this.state.isLoading} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,33 +3,10 @@ import UserItem from './UserItem'
 
 class Users extends Component {
 
-    state = {
-        users: [
-            {
-                "id": 1,
-                "login": "pravin-sharma",
-                "avatar_url": "https://avatars.githubusercontent.com/u/32671972?v=4",
-                "html_url": "https://github.com/pravin-sharma",
-            },
-            {
-                "id": 2,
-                "login": "mavi888",
-                "avatar_url": "https://avatars.githubusercontent.com/u/7993283?v=4",
-                "html_url": "https://github.com/mavi888"
-            },
-            {
-                "id": 3,
-                "login": "andrewjmead",
-                "avatar_url": "https://avatars.githubusercontent.com/u/2992789?v=4",
-                "html_url": "https://github.com/andrewjmead",
-            }
-        ]
-    }
-
     render() {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => (
+                {this.props.users.map(user => (
                     <UserItem key={user.id} user={user} />
                 )
                 )}
