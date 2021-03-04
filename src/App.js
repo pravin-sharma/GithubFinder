90cb0e6b25d203e8a08c014bdbbbed54797c95a9
Pravin Sharma <Pravin.Sharma@lntinfotech.com>
2021-03-04 22:12:16 +0530

    Added search component with api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+
 import './App.css';
+
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
-import axios from 'axios';
+import Search from './components/users/Search';
+
 
 
 class App extends Component {
@@ -14,24 +18,28 @@ class App extends Component {
 
   async componentDidMount() {
     this.setState({ isLoading: true });
-
-    const result = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-
+    const result = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ users: result.data, isLoading: false });
-
   }
+
+  searchUser = async (text) =>{
+    this.setState({ isLoading: true });
+    const result = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: result.data.items, isLoading: false });
+  } 
 
   render() {
     return (
       <div className="App" >
         < Navbar />
+
         <div className='container'>
+          <Search searchUser={this.searchUser}/>
           <Users users={this.state.users} isLoading={this.state.isLoading} />
         </div>
       </div>
     );
   }
-
 }
 
 export default App;
---
+++ src/components/users/Search.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export class Search extends Component {
+
+    state = {
+        text: ''
+    };
+
+    onChange = (event) => {
+        this.setState({[event.target.name]: event.target.value});
+    }   
+
+    onSubmit = (event) =>{
+        event.preventDefault();
+        //pass as a prop to App.js and search in App.js
+        this.props.searchUser(this.state.text)
+
+        this.setState({text: ''});
+    }
+
+    render() {
+        return (
+            <div>
+                <form className="form" onSubmit={this.onSubmit}>
+                <input type="text" name="text" placeholder="Search User.." onChange={this.onChange} value={this.state.text}/>
+                <input type="submit" value="Search" className="btn btn-dark btn-block"/>
+            </form>
+            </div>
+        )
+    }
+}
+
+export default Search
